2454516d67a90472498b30ab70fb42f1bd0a3734
Talha <talha7171@gmail.com>
2018-11-14 18:51:12 -0500

    Updated beans
    Still should change file names to ItemBean and CatagoryBean

--- B2C/src/model/catalog/Category.java
+++ B2C/src/model/catalog/Category.java
@@ -1,5 +1,59 @@
 package model.catalog;
 
 public class Category {
+	
+	private String name, description;
+	private int id;
+	private String picture;
+	
+	Category() {
+	}
+	
+	Category(String name, String description, int id, String picture) {
+		setName(name);
+		setDescription(description);
+		setId(id);
+		setPicture(picture);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getPicture() {
+		return picture;
+	}
+	
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}	
+	
+	
+	public String toString() {
+		return String.format("Name: %s | Description: %s | ID: %s | Picture: %s \n", name, description, id, picture);
+		//fix this
+	}
+	
+	
 
 }
--- B2C/src/model/catalog/Item.java
+++ B2C/src/model/catalog/Item.java
@@ -1,5 +1,67 @@
 package model.catalog;
 
 public class Item {
+	
+	private String name;
+	private double price;
+	private int quantity, number, catID;
+	
+	Item() {
+	}
+	
+	Item(String name, double price, int quantity, int number) {
+		setName(name);
+		setPrice(price);
+		setQuantity(quantity);
+		setNumber(number);
+		//setCatID(catID);
+		//can add catID if needed but for now leave it out
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}	
+	
+	public int getCatID() {
+		return catID;
+	}
+	
+	public void setCatID(int catID) {
+		this.catID = catID;
+	}	
+	
+	public String toString() {
+		return String.format("Name: %s | Price: %s | Quantity: %s | Number: %s \n", name, price, quantity, number);
+	}
+	
+	
 
 }
